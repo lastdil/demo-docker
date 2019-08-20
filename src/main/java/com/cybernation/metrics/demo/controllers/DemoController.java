dfea6753812ca017f30e00814cf7e50c179418cd
Ivan Drygin <drygin@gmail.com>
2019-08-21 00:35:16 +0300

    factorial added

--- src/main/java/com/cybernation/metrics/demo/controllers/DemoController.java
+++ src/main/java/com/cybernation/metrics/demo/controllers/DemoController.java
@@ -1,14 +1,28 @@
 package com.cybernation.metrics.demo.controllers;
 
+import com.cybernation.metrics.demo.service.FactorialService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 import java.util.UUID;
 
 @RestController
 public class DemoController {
 
+    @Autowired
+    FactorialService factorialService;
+
     @GetMapping("/demo")
     public String demo() {
         return UUID.randomUUID().toString();
+    }
+
+    @GetMapping("/test")
+    public int factorial() {
+        Random r = new Random();
+        int n = r.nextInt((50 - 5) + 1) + 5;
+        return factorialService.calculateFactorial(n);
     }
 }
---
+++ src/main/java/com/cybernation/metrics/demo/service/FactorialService.java
@@ -0,0 +1,14 @@
+package com.cybernation.metrics.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FactorialService {
+    public int calculateFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+        return result;
+    }
+}
